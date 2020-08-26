f2b37a51ba66f0e965f844494020d1d0ac66408c
EggplantHero <jessetristanweber@gmail.com>
2020-08-26 19:52:09 -0400

    fixed navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -8,18 +8,7 @@ const NavBar = ({ user }) => {
       <Link className="navbar-brand" to="/">
         Spellbook
       </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-collapse">
         <div className="navbar-nav">
           <NavLink className="nav-item nav-link" to="/spells">
             Spells
